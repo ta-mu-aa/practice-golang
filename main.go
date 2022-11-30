a005e2a82938dce49519203c0a47f9621e5d7987
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-11-30 23:49:49 +0900

    便利な標準パッケージ

--- main.go
+++ main.go
@@ -1,21 +1,58 @@
 package main
 
 import (
-	"sync"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"sort"
+	"time"
 )
 
-func producer(ch chan int, i int){
-	ch <- i * 2
-}
-
-
 func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int)
+	// 時間関係
+	t := time.Now()
+	fmt.Println(t.Format(time.RFC3339))
+	fmt.Println(t.Year(), t.Month())
 
-	// Producer 
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go producer(ch, i)
+	// 正規表現
+	match, _ := regexp.MatchString("a([a-z]+)e", "apple")
+	fmt.Println(match)
+
+	r := regexp.MustCompile("a([a-z]+)e")
+	ms := r.MatchString("apple")
+	fmt.Println(ms)
+
+	// ソート
+	i := []int{5, 3, 2, 1, 4}
+	s := []string{"d", "a", "f"}
+	p := []struct {
+		Name string
+		Age  int
+	}{
+		{"Nancy", 20},
+		{"Vera", 40},
+		{"Mike", 30},
+		{"Bob", 50},
 	}
+	fmt.Println(i, s, p)
+	sort.Ints(i)
+	sort.Strings(s)
+	sort.Slice(p, func(i, j int) bool { return p[i].Age < p[j].Age })
+	fmt.Println(i, s, p)
+
+	// iota
+	const (
+		c1 = iota
+		c2 = iota
+		c3 = iota
+	)
+	fmt.Println(c1, c2, c3)
+
+	// ioutil
+	content, err := ioutil.ReadFile("main.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(content))
 }
