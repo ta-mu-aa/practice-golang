8b54805c46047cb1fc1d9b29f3b0a11204c8a84f
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-01 21:51:59 +0900

    http通信のライブラリ

--- main.go
+++ main.go
@@ -3,56 +3,29 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
-	"regexp"
-	"sort"
-	"time"
+	"net/http"
+	"net/url"
 )
 
 func main() {
-	// 時間関係
-	t := time.Now()
-	fmt.Println(t.Format(time.RFC3339))
-	fmt.Println(t.Year(), t.Month())
+	// resp, _ := http.Get("http://example.com")
+	// defer resp.Body.Close()
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
 
-	// 正規表現
-	match, _ := regexp.MatchString("a([a-z]+)e", "apple")
-	fmt.Println(match)
+	base, _ := url.Parse("http://example.com/")
+	reference, _ := url.Parse("/test?a=1&b=2")
+	endpoint := base.ResolveReference(reference).String()
+	req, _ := http.NewRequest("GET", endpoint, nil)
+	req.Header.Add("If-None-Match", `W/"wyzzy"`)
+	q := req.URL.Query()
+	q.Add("c", "3&%")
+	fmt.Println(q)
+	fmt.Println(q.Encode())
+	req.URL.RawQuery = q.Encode()
 
-	r := regexp.MustCompile("a([a-z]+)e")
-	ms := r.MatchString("apple")
-	fmt.Println(ms)
-
-	// ソート
-	i := []int{5, 3, 2, 1, 4}
-	s := []string{"d", "a", "f"}
-	p := []struct {
-		Name string
-		Age  int
-	}{
-		{"Nancy", 20},
-		{"Vera", 40},
-		{"Mike", 30},
-		{"Bob", 50},
-	}
-	fmt.Println(i, s, p)
-	sort.Ints(i)
-	sort.Strings(s)
-	sort.Slice(p, func(i, j int) bool { return p[i].Age < p[j].Age })
-	fmt.Println(i, s, p)
-
-	// iota
-	const (
-		c1 = iota
-		c2 = iota
-		c3 = iota
-	)
-	fmt.Println(c1, c2, c3)
-
-	// ioutil
-	content, err := ioutil.ReadFile("main.go")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(content))
+	var client *http.Client = &http.Client{}
+	resp, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
