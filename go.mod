23f5b5a6e537e7d77446af462869b550b50127a5
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-03 13:01:01 +0900

    データベース操作について

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module sample-app
 
 go 1.19
 
-require golang.org/x/sync v0.1.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,33 +1,72 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
+	"database/sql"
+	"log"
 
-	"golang.org/x/sync/semaphore"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-var s *semaphore.Weighted = semaphore.NewWeighted(1)
-
-func longProcess(ctx context.Context) {
-	isAcquire := s.TryAcquire(1)
-	if !isAcquire {
-		fmt.Println("Could not get lock")
-		return
-	}
-	defer s.Release(1)
-	fmt.Println("Wait...")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Done")
+type Person struct {
+	Name string
+	Age int
 }
 
-func main() {
-	ctx := context.TODO()
-	go longProcess(ctx)
-	go longProcess(ctx)
-	go longProcess(ctx)
-	time.Sleep(2 * time.Second)
-	go longProcess(ctx)
-	time.Sleep(2 * time.Second)
+func main(){
+	Dbconnection, _ := sql.Open("sqlite3", "./example.sql")
+	defer Dbconnection.Close()
+	cmd := `CREATE TABLE IF NOT EXISTS person (
+						name STRING,
+						age INT)`
+	_, err := Dbconnection.Exec(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
+	// _, err = Dbconnection.Exec(cmd, "Nancy",20)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// cmd = "UPDATE person SET age = ? WHERE name = ?"
+	// _, err = Dbconnection.Exec(cmd, 25, "Mike")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+// 	cmd = "SELECT * FROM person"
+// 	rows, _ := Dbconnection.Query(cmd)
+// 	defer rows.Close()
+// 	var pp []Person
+// 	for rows.Next(){
+// 		var p Person
+// 		err := rows.Scan(&p.Name, &p.Age)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 		pp = append(pp, p)
+// 	}
+// 	for _, p := range pp{
+// 		fmt.Println(p.Name, p.Age)
+// 	}
+
+	// cmd = "SELECT * FROM person where age = ?"
+	// row := Dbconnection.QueryRow(cmd, 20)
+	// var p Person
+	// err = row.Scan(&p.Name, &p.Age)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows{
+	// 		log.Println("No row")
+	// 	}else{
+	// 		log.Println(err)
+	// 	}
+	// }
+	// fmt.Println(p.Name, p.Age)
+
+	cmd = "DELETE FROM person where name = ?"
+	_, err = Dbconnection.Exec(cmd, "Nancy")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
