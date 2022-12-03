8ad5a1f7f91963074af40da83e9df09cf1f8f422
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-03 22:14:58 +0900

    httpListenAndServerとhttpResponseWriteとhttp.Requestとtemplate

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"text/template"
 )
 
 type Page struct {
@@ -24,10 +26,28 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body: body}, nil
 }
 
-func main(){
-	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
-	p1.save()
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
 
-	p2, _ := loadPage(p1.Title)
-	fmt.Println(string(p2.Body))
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func main(){
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
