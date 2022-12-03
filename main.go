0e33d69acac72817d385187c1b014a84774a6215
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-03 21:05:24 +0900

    Webアプリ1-ioutil

--- main.go
+++ main.go
@@ -1,72 +1,33 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"fmt"
+	"io/ioutil"
 )
 
-type Person struct {
-	Name string
-	Age int
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error){
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil{
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func main(){
-	Dbconnection, _ := sql.Open("sqlite3", "./example.sql")
-	defer Dbconnection.Close()
-	cmd := `CREATE TABLE IF NOT EXISTS person (
-						name STRING,
-						age INT)`
-	_, err := Dbconnection.Exec(cmd)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+	p1.save()
 
-	// cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
-	// _, err = Dbconnection.Exec(cmd, "Nancy",20)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
-	// cmd = "UPDATE person SET age = ? WHERE name = ?"
-	// _, err = Dbconnection.Exec(cmd, 25, "Mike")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
-// 	cmd = "SELECT * FROM person"
-// 	rows, _ := Dbconnection.Query(cmd)
-// 	defer rows.Close()
-// 	var pp []Person
-// 	for rows.Next(){
-// 		var p Person
-// 		err := rows.Scan(&p.Name, &p.Age)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 		pp = append(pp, p)
-// 	}
-// 	for _, p := range pp{
-// 		fmt.Println(p.Name, p.Age)
-// 	}
-
-	// cmd = "SELECT * FROM person where age = ?"
-	// row := Dbconnection.QueryRow(cmd, 20)
-	// var p Person
-	// err = row.Scan(&p.Name, &p.Age)
-	// if err != nil {
-	// 	if err == sql.ErrNoRows{
-	// 		log.Println("No row")
-	// 	}else{
-	// 		log.Println(err)
-	// 	}
-	// }
-	// fmt.Println(p.Name, p.Age)
-
-	cmd = "DELETE FROM person where name = ?"
-	_, err = Dbconnection.Exec(cmd, "Nancy")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
 }
