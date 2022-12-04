26892940823e768d98e7a32a517ce4ba27b2afbd
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-04 23:41:08 +0900

    サードパッケージ ini

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sample-app
 go 1.19
 
 require (
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/go-ini/ini"
 )
 
+type ConfigList struct {
+	Port 			int
+	DbName 		string
+	SLDriver	string
+}
+
+var Config ConfigList
+
+func init(){
+	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList{
+		Port: cfg.Section("web").Key("port").MustInt(),
+		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
+		SLDriver: cfg.Section("db").Key("driver").String(),
+	}
+}
+
 func main(){
+	fmt.Printf("%T %v\n", Config.Port, Config.Port)
+	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
+	fmt.Printf("%T %v\n", Config.SLDriver, Config.SLDriver)
 }
