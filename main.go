cea46d9cf0a0a3e74ea121a05ad3298a4698e002
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-12-05 23:52:40 +0900

    パッケージcontextについて

--- main.go
+++ main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
-
-	"github.com/go-ini/ini"
+	"time"
 )
 
-type ConfigList struct {
-	Port 			int
-	DbName 		string
-	SLDriver	string
-}
-
-var Config ConfigList
-
-func init(){
-	cfg, _ := ini.Load("config.ini")
-	Config = ConfigList{
-		Port: cfg.Section("web").Key("port").MustInt(),
-		DbName: cfg.Section("db").Key("name").MustString("example.sql"),
-		SLDriver: cfg.Section("db").Key("driver").String(),
-	}
+func longProcess(ctx context.Context, ch chan string){
+	fmt.Println("run")
+	time.Sleep(2 * time.Second)
+	fmt.Println("finish")
+	ch <- "result"
 }
 
 func main(){
-	fmt.Printf("%T %v\n", Config.Port, Config.Port)
-	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
-	fmt.Printf("%T %v\n", Config.SLDriver, Config.SLDriver)
+	ch := make(chan string)
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1 * time.Second)
+	defer cancel()
+	go longProcess(ctx, ch)
+
+	for {
+		select {
+		case <- ctx.Done():
+			fmt.Println(ctx.Err())
+			return
+		case <- ch:
+			fmt.Println("success")
+			return
+		}
+	}
 }
